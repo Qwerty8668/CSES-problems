ec4de129b95679e7891061e93a4289081a7e92b1
Qwerty8668 <81041923+Qwerty8668@users.noreply.github.com>
2024-02-03 19:05:44 +0100

    Add files via upload

---
+++ Introductory Problems/weird algorithm.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    long long int n;
+    scanf("%lld", &n);
+
+    printf("%lld ", n);
+
+    while(n>1) {
+        if(n%2 == 0) n/=2;
+        else n = n*3 +1;
+        printf("%lld ", n);
+    }
+
+    return 0;
+}
